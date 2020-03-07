c97a41ce55eb90a51390f2a1505395a8ec38ccc8
LukeDunsmore <luke.dunsmore@hotmail.co.uk>
2020-03-07 10:17:35 +0000

    file to issue commands to robots

---
+++ ros/full_coverage.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+def run(args):
+    rospy.init_mode('full_coverage')
+
+    # Update control at 100Hz
+    rate_limiter = rospy.Rate(100)
+    publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+
+    previous_time = rospy.Time.now()
+
+    i = 0
+    while i < 10 and not rospy.is_shutdown():
+        publisher.publish(stop_msg)
+        rate_limiter.sleep()
+        i += 1
+
+    while not rospy.is_shutdown():
+        current_time = rospy.Time.now().to_sec()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Runs Full Coverage naviation')
+  args, unknown = parser.parse_known_args()
+  try:
+    run(args)
+  except rospy.ROSInterruptException:
+    pass
